71bda6b94a0b2781d05597ddc78face3f0fdba06
karlie.tang <karlie.tang@ucloud.cn>
2021-04-06 14:50:06 +0800

    main3 upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 #coding=utf-8
 
-print ("Life is short, you need Python !")
-
+a = 10
+b = 20
+c = a + b
+#print ("Life is short, you need Python !")
+print("the value of c is  ", c)
